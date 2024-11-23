58bffabf8282e6e34cea29698dced2a0ea47ae91
Vitaly Belik <vitaly.belik@gmail.com>
2024-11-23 09:54:27 +0300

    changed linter to google

--- src/main.ts
+++ src/main.ts
@@ -52,9 +52,9 @@ const pages = {
 };
 
 Handlebars.registerHelper({
-  eq: (v1, v2) => v1 === v2,
-  ne: (v1, v2) => v1 !== v2,
-  lt: (v1, v2) => v1 < v2,
+  eq: (v1: unknown, v2: unknown) => v1 === v2,
+  ne: (v1: unknown, v2: unknown) => v1 !== v2,
+  lt: (v1: string, v2: string) => v1 < v2,
   gt: (v1, v2) => v1 > v2,
   lte: (v1, v2) => v1 <= v2,
   gte: (v1, v2) => v1 >= v2,
