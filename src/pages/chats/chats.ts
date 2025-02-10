88acfa71ab4a547b6ff574c45c6b1108dc6dea44
Vitaly Belik <vitaly.belik@gmail.com>
2025-02-10 22:38:42 +0300

    chats tbc

--- src/pages/chats/chats.ts
+++ src/pages/chats/chats.ts
@@ -53,7 +53,7 @@ export default class ChatsPage extends Block {
           {{#if error}}<div class="input__error ">{{error}}</div>{{/if}}
         </div>
 
-        <hr class="divider">
+        <hr class="nav_divider">
         <div class="parent">
           <div class="child">
             <div class="grand-child one">
@@ -74,7 +74,7 @@ export default class ChatsPage extends Block {
           </div>
         </div>
 
-        <hr class="divider">
+        <hr class="nav_divider">
         <div class="parent">
           <div class="child">
             <div class="grand-child one">
@@ -95,8 +95,8 @@ export default class ChatsPage extends Block {
           </div>
         </div>
 
-        <hr class="divider">
-        <div class="parent">
+        <hr class="nav_divider">
+        <div class="parent__selected">
           <div class="child">
             <div class="grand-child one">
               <img src="src/assets/imgs/img_avatar.png" alt="Avatar" class="item__avatar">
@@ -116,13 +116,11 @@ export default class ChatsPage extends Block {
           </div>
         </div>
 
-
-
-        <hr class="divider">
+        <hr class="nav_divider">
       </div>
 
       <div class="chat">
-        <div class="header">
+        <div class="chat__header">
           <div class="header__avatar">
             <img src="src/assets/imgs/img_avatar.png" alt="Avatar" class="avatar_img">
           </div>
@@ -131,17 +129,37 @@ export default class ChatsPage extends Block {
           </div>
           <div class="dots"></div>
         </div>
-        <hr class="divider">
 
+        <div class="messages__container">
+          <div class ="message__date">19 января</div>
+          <div class="message__counterparty__text">
+            message from the counterparty
+            <div class="message__time">
+              11:34
+            </div>
+          </div>
+          <div class="message__counterparty__img">
+            <img src="src/assets/imgs/counterparty_img.jpg" alt="Avatar" class="message_img">
+            <div class="message__time">
+              11:34
+            </div>
+          </div>
 
-        <div class ="message_date">19 января</div>
-
-
-        <div class="message_counterparty">
-          message from the counterparty
-        </div>
-        <div class="message_user">
-          message from the userx
+          <div class="message__user">
+            message from the userx
+            message from the userx
+            message from the userx
+            message from the userx
+            message from the userx
+            message from the userx
+            message from the userx
+            message from the userx
+            message from the userx
+            <div class="message__time">
+              <svg class="reading_status" viewBox="0 0 512 512"><path fill="currentColor" d="M505 174.8l-39.6-39.6c-9.4-9.4-24.6-9.4-33.9 0L192 374.7 80.6 263.2c-9.4-9.4-24.6-9.4-33.9 0L7 302.9c-9.4 9.4-9.4 24.6 0 34L175 505c9.4 9.4 24.6 9.4 33.9 0l296-296.2c9.4-9.5 9.4-24.7 .1-34zm-324.3 106c6.2 6.3 16.4 6.3 22.6 0l208-208.2c6.2-6.3 6.2-16.4 0-22.6L366.1 4.7c-6.2-6.3-16.4-6.3-22.6 0L192 156.2l-55.4-55.5c-6.2-6.3-16.4-6.3-22.6 0L68.7 146c-6.2 6.3-6.2 16.4 0 22.6l112 112.2z"/></svg>
+              11:34
+            </div>
+          </div>
         </div>
 
 
