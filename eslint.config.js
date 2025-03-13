9994d43afced9ba2edc54c3d8f4690f0d9fce5aa
Vitaly Belik <vitaly.belik@gmail.com>
2025-03-13 19:37:37 +0300

    es1

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -8,4 +9,5 @@ export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
 ];
