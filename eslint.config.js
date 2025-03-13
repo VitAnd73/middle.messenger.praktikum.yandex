58626824e658a8dac9f5b46f89d430a2bfce4d64
Vitaly Belik <vitaly.belik@gmail.com>
2025-03-13 15:53:05 +0300

    changed esling config

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {ignores: [".config/*", "server.cjs", "coverage/*"]},
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
